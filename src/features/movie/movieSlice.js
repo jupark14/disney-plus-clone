04b376abde096f05807b825884425d420864e8e9
jupark14 <25th_night@kakao.com>
2021-06-07 22:26:45 +0900

    Applied Redux to Detail, Home, Movies.js

--- src/features/movie/movieSlice.js
+++ src/features/movie/movieSlice.js
@@ -6,7 +6,9 @@ const initialState = {
 
 const movieSlice = createSlice({
   name: "movie",
-  initialState,
+  initialState: {
+    movies: [],
+  },
   reducers: {
     setMovies: (state, action) => {
       state.movies = action.payload;
@@ -14,10 +16,10 @@ const movieSlice = createSlice({
   },
 });
 
-//action을 쓸 수 있도록 밖으로 보내는 역할
+//action을 쓸 수 있도록 밖으로 보내는 역할. createSlice를 쓰면 action함수를 자동으로 생성해주고, reducer에서 지었던 setMovies로 내보낼 수 있다는 것
 export const { setMovies } = movieSlice.actions;
 
-//state를 쓸 수 있게 밖으로 보내는 역할
+//initialState에 movies도 있고 fantasticMovies도 있다고 치면 그 중에서 movies를 가져오는 것을 편하게 하기 위해서 씀
 export const selectMovies = (state) => state.movie.movies;
 
 //reducer는 store에게 reducer가 있다고 전하기 위해 밖으로 보냄
